d595034a34734b36411efa425c2b5c494e16d162
rapidfireworks <rapidfireworks@gmail.com>
2020-04-02 13:04:46 +0900

    Solve

--- secret-handshake/lib/secret_handshake.ex
+++ secret-handshake/lib/secret_handshake.ex
@@ -13,7 +13,28 @@ defmodule SecretHandshake do
 
   10000 = Reverse the order of the operations in the secret handshake
   """
+  use Bitwise
+
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
+    sequence(code)
+    |> Enum.filter(&bitwise_and(code, &1))
+    |> Enum.map(&command/1)
   end
+
+  defp sequence(code) do
+    if bitwise_and(code, 16) do
+      [8, 4, 2, 1]
+    else
+      [1, 2, 4, 8]
+    end
+  end
+
+  defp command(code)
+  defp command(1), do: "wink"
+  defp command(2), do: "double blink"
+  defp command(4), do: "close your eyes"
+  defp command(8), do: "jump"
+
+  defp bitwise_and(left, right), do: band(left, right) > 0
 end
