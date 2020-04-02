3b4bf586ba5e53871be9847adddbaa80ec8480fc
rapidfireworks <rapidfireworks@gmail.com>
2020-04-02 09:58:24 +0900

    Solve

--- rna-transcription/lib/rna_transcription.ex
+++ rna-transcription/lib/rna_transcription.ex
@@ -8,6 +8,13 @@ defmodule RnaTranscription do
   'UGAC'
   """
   @spec to_rna([char]) :: [char]
-  def to_rna(dna) do
-  end
+  def to_rna(dna)
+  def to_rna(''), do: ''
+  def to_rna([transcription|tail]), do: [rna_complement(transcription)|to_rna(tail)]
+
+  defp rna_complement(transcription)
+  defp rna_complement(?G), do: ?C
+  defp rna_complement(?C), do: ?G
+  defp rna_complement(?T), do: ?A
+  defp rna_complement(?A), do: ?U
 end
