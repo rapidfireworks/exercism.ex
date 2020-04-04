a1a97477b4acbf025992a777fd2faf56fd3896e8
rapidfireworks <rapidfireworks@gmail.com>
2020-04-04 21:00:54 +0900

    Solve

--- protein-translation/lib/protein_translation.ex
+++ protein-translation/lib/protein_translation.ex
@@ -4,6 +4,19 @@ defmodule ProteinTranslation do
   """
   @spec of_rna(String.t()) :: {atom, list(String.t())}
   def of_rna(rna) do
+    to_charlist(rna)
+    |> translate
+  end
+
+  defp translate(''), do: {:ok, []}
+  defp translate(rna) do
+    {head, tail} = Enum.split(rna, 3)
+
+    case {of_codon(to_string(head)), translate(tail)} do
+      {{:ok, "STOP"}, _} -> {:ok, []}
+      {{:ok, protein}, {:ok, proteins}} -> {:ok, [protein | proteins]}
+      _ -> {:error, "invalid RNA"}
+    end
   end
 
   @doc """
@@ -28,6 +41,22 @@ defmodule ProteinTranslation do
   UGA -> STOP
   """
   @spec of_codon(String.t()) :: {atom, String.t()}
-  def of_codon(codon) do
-  end
+  def of_codon("UGC"), do: {:ok, "Cysteine"}
+  def of_codon("UGU"), do: {:ok, "Cysteine"}
+  def of_codon("UUA"), do: {:ok, "Leucine"}
+  def of_codon("UUG"), do: {:ok, "Leucine"}
+  def of_codon("AUG"), do: {:ok, "Methionine"}
+  def of_codon("UUU"), do: {:ok, "Phenylalanine"}
+  def of_codon("UUC"), do: {:ok, "Phenylalanine"}
+  def of_codon("UCU"), do: {:ok, "Serine"}
+  def of_codon("UCC"), do: {:ok, "Serine"}
+  def of_codon("UCA"), do: {:ok, "Serine"}
+  def of_codon("UCG"), do: {:ok, "Serine"}
+  def of_codon("UGG"), do: {:ok, "Tryptophan"}
+  def of_codon("UAU"), do: {:ok, "Tyrosine"}
+  def of_codon("UAC"), do: {:ok, "Tyrosine"}
+  def of_codon("UAA"), do: {:ok, "STOP"}
+  def of_codon("UAG"), do: {:ok, "STOP"}
+  def of_codon("UGA"), do: {:ok, "STOP"}
+  def of_codon(_codon), do: {:error, "invalid codon"}
 end
