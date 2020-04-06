819994977af1b8c7c926c84c17410c6536b43d0b
rapidfireworks <rapidfireworks@gmail.com>
2020-04-06 10:04:05 +0900

    Solve

--- acronym/lib/acronym.ex
+++ acronym/lib/acronym.ex
@@ -5,5 +5,17 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
+    Regex.split(~R/[\p{Z}-_]+/, string)
+    |> Enum.map(&Regex.replace(~R/[^\p{L}]/, &1, ""))
+    |> Enum.map(&shorten/1)
+    |> Enum.join()
+  end
+
+  defp shorten(letter) do
+    cond do
+      Regex.match?(~R/^\p{Ll}/, letter) -> String.at(letter, 0) |> String.upcase()
+      Regex.match?(~R/^\p{Lu}+$/, letter) -> String.at(letter, 0)
+      true -> Regex.replace(~R/\p{Ll}/, letter, "")
+    end
   end
 end
