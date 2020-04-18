04cf56c01bc3763e1f2936280d7add1493ddf53c
rapidfireworks <rapidfireworks@gmail.com>
2020-04-18 11:27:15 +0900

    Refactor with POSIX bracket regex

--- acronym/lib/acronym.ex
+++ acronym/lib/acronym.ex
@@ -5,17 +5,17 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
-    Regex.split(~R/[\p{Z}-_]+/, string)
-    |> Enum.map(&Regex.replace(~R/[^\p{L}]/, &1, ""))
+    Regex.split(~R/[[:space:]]+/u, string, trim: true)
+    |> Enum.map(&Regex.replace(~R/[^[:alpha:]]+/, &1, ""))
     |> Enum.map(&shorten/1)
     |> Enum.join()
   end
 
   defp shorten(letter) do
     cond do
-      Regex.match?(~R/^\p{Ll}/, letter) -> String.at(letter, 0) |> String.upcase()
-      Regex.match?(~R/^\p{Lu}+$/, letter) -> String.at(letter, 0)
-      true -> Regex.replace(~R/\p{Ll}/, letter, "")
+      Regex.match?(~R/^[[:lower:]]/u, letter) -> String.at(letter, 0) |> String.upcase()
+      Regex.match?(~R/^[[:upper:]]+$/u, letter) -> String.at(letter, 0)
+      true -> Regex.replace(~R/[[:lower:]]+/u, letter, "")
     end
   end
 end
