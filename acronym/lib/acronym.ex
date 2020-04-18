54e013afd0cff2a6ab4f70982a18e531987f7c78
rapidfireworks <rapidfireworks@gmail.com>
2020-04-18 13:11:11 +0900

    Refactor with Regex.replace/3

--- acronym/lib/acronym.ex
+++ acronym/lib/acronym.ex
@@ -5,17 +5,8 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
-    Regex.split(~R/[[:space:]]+/u, string, trim: true)
-    |> Enum.map(&Regex.replace(~R/[^[:alpha:]]+/, &1, ""))
-    |> Enum.map(&shorten/1)
-    |> Enum.join()
-  end
-
-  defp shorten(letter) do
-    cond do
-      Regex.match?(~R/^[[:lower:]]/u, letter) -> String.at(letter, 0) |> String.upcase()
-      Regex.match?(~R/^[[:upper:]]+$/u, letter) -> String.at(letter, 0)
-      true -> Regex.replace(~R/[[:lower:]]+/u, letter, "")
-    end
+    ~R/([[:alpha:]])(?:[[:upper:]]+|[[:lower:]']*)[^[:alpha:]']*/
+    |> Regex.replace(string, "\\1")
+    |> String.upcase()
   end
 end
