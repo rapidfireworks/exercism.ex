9967f776e73f4eb49456060649120dc427fd88e2
rapidfireworks <rapidfireworks@gmail.com>
2020-04-19 13:28:09 +0900

    Solve

--- dnd-character/lib/dnd_character.ex
+++ dnd-character/lib/dnd_character.ex
@@ -12,14 +12,26 @@ defmodule DndCharacter do
   defstruct ~w[strength dexterity constitution intelligence wisdom charisma hitpoints]a
 
   @spec modifier(pos_integer()) :: integer()
-  def modifier(score) do
-  end
+  def modifier(score), do: Integer.floor_div(score - 10, 2)
 
   @spec ability :: pos_integer()
   def ability do
+    dies = Enum.take_random(1..6, 4)
+    Enum.sum(dies) - Enum.min(dies)
   end
 
   @spec character :: t()
   def character do
+    constitution = ability()
+
+    %DndCharacter{
+      strength: ability(),
+      dexterity: ability(),
+      constitution: constitution,
+      intelligence: ability(),
+      wisdom: ability(),
+      charisma: ability(),
+      hitpoints: 10 + modifier(constitution)
+    }
   end
 end
