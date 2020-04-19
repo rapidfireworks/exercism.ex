92c1a9935c197160d5dc66314d1023eb3bd1fb40
rapidfireworks <rapidfireworks@gmail.com>
2020-04-19 12:37:54 +0900

    Solve

--- resistor-color/lib/resistor_color.ex
+++ resistor-color/lib/resistor_color.ex
@@ -3,9 +3,29 @@ defmodule ResistorColor do
 
   @spec colors() :: list(String.t())
   def colors do
+    [
+      "black",
+      "brown",
+      "red",
+      "orange",
+      "yellow",
+      "green",
+      "blue",
+      "violet",
+      "grey",
+      "white"
+    ]
   end
 
   @spec code(String.t()) :: integer()
-  def code(color) do
-  end
+  def code("black"), do: 0
+  def code("brown"), do: 1
+  def code("red"), do: 2
+  def code("orange"), do: 3
+  def code("yellow"), do: 4
+  def code("green"), do: 5
+  def code("blue"), do: 6
+  def code("violet"), do: 7
+  def code("grey"), do: 8
+  def code("white"), do: 9
 end
