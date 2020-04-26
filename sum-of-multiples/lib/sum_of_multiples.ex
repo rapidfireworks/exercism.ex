ea53610a02201bd661dfeff40f70723f3db9c745
rapidfireworks <rapidfireworks@gmail.com>
2020-04-26 17:34:28 +0900

    Solve

--- sum-of-multiples/lib/sum_of_multiples.ex
+++ sum-of-multiples/lib/sum_of_multiples.ex
@@ -4,5 +4,14 @@ defmodule SumOfMultiples do
   """
   @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
   def to(limit, factors) do
+    Stream.map(factors, &multiples(limit, &1))
+    |> Enum.reduce(MapSet.new(), &MapSet.union/2)
+    |> Enum.sum()
+  end
+
+  defp multiples(limit, factor) do
+    Stream.iterate(factor, &(&1 + factor))
+    |> Enum.take_while(&(&1 < limit))
+    |> MapSet.new()
   end
 end
