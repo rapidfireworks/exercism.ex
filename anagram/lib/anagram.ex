ea4d04d0372833b32e5f8a77ee08ed800008ded3
rapidfireworks <rapidfireworks@gmail.com>
2020-04-30 13:17:28 +0900

    Solve

--- anagram/lib/anagram.ex
+++ anagram/lib/anagram.ex
@@ -4,5 +4,21 @@ defmodule Anagram do
   """
   @spec match(String.t(), [String.t()]) :: [String.t()]
   def match(base, candidates) do
+    base = String.downcase(base)
+
+    candidates
+    |> Enum.filter(&anagram?(base, &1))
+  end
+
+  defp anagram?(base, candidate) do
+    candidate = String.downcase(candidate)
+
+    !String.equivalent?(base, candidate) and
+      Map.equal?(histogram(base), histogram(candidate))
+  end
+
+  defp histogram(word) do
+    String.graphemes(word)
+    |> Enum.frequencies()
   end
 end
