b2cd9d52fbc3415efcc82c63fc40213447771058
rapidfireworks <rapidfireworks@gmail.com>
2020-05-03 19:41:16 +0900

    Solve

--- hamming/lib/hamming.ex
+++ hamming/lib/hamming.ex
@@ -8,6 +8,12 @@ defmodule Hamming do
   {:ok, 4}
   """
   @spec hamming_distance([char], [char]) :: {:ok, non_neg_integer} | {:error, String.t()}
-  def hamming_distance(strand1, strand2) do
-  end
+  def hamming_distance(strand1, strand2), do: diff(strand1, strand2)
+
+  defp diff(strand1, strand2, result \\ 0)
+  defp diff([], [], result), do: {:ok, result}
+  defp diff([], _strand, _result), do: {:error, "Lists must be the same length"}
+  defp diff(_strand, [], _result), do: {:error, "Lists must be the same length"}
+  defp diff([base | strand1], [base | strand2], result), do: diff(strand1, strand2, result)
+  defp diff([_lhs | strand1], [_rhs | strand2], result), do: diff(strand1, strand2, result + 1)
 end
