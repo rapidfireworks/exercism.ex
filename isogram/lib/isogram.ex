a3c87fe3e8698099b551bf1ffc099df645d1a1c9
rapidfireworks <rapidfireworks@gmail.com>
2020-05-06 14:13:29 +0900

    Solve

--- isogram/lib/isogram.ex
+++ isogram/lib/isogram.ex
@@ -4,5 +4,10 @@ defmodule Isogram do
   """
   @spec isogram?(String.t()) :: boolean
   def isogram?(sentence) do
+    String.downcase(sentence)
+    |> String.replace(~R/[^[:alpha:]]/u, "")
+    |> String.graphemes()
+    |> Enum.frequencies()
+    |> Enum.all?(&(elem(&1, 1) === 1))
   end
 end
