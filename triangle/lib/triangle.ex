b390ca80f5602e49a2bf22923c4701132f7984b0
rapidfireworks <rapidfireworks@gmail.com>
2020-05-06 13:55:58 +0900

    Solve

--- triangle/lib/triangle.ex
+++ triangle/lib/triangle.ex
@@ -5,6 +5,12 @@ defmodule Triangle do
   Return the kind of triangle of a triangle with 'a', 'b' and 'c' as lengths.
   """
   @spec kind(number, number, number) :: {:ok, kind} | {:error, String.t()}
-  def kind(a, b, c) do
-  end
+  def kind(a, b, c) when a > 0 and b > 0 and c > 0, do: Enum.sort([a, b, c]) |> kind()
+  def kind(_a, _b, _c), do: {:error, "all side lengths must be positive"}
+
+  defp kind([a, a, a]), do: {:ok, :equilateral}
+  defp kind([a, b, c]) when a + b <= c, do: {:error, "side lengths violate triangle inequality"}
+  defp kind([a, a, _c]), do: {:ok, :isosceles}
+  defp kind([_a, b, b]), do: {:ok, :isosceles}
+  defp kind([_a, _b, _c]), do: {:ok, :scalene}
 end
