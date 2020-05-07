831a5b3c0ffe1795e266fa0d4c142ae69a3413b0
rapidfireworks <rapidfireworks@gmail.com>
2020-05-07 09:56:45 +0900

    Solve

--- grade-school/lib/school.ex
+++ grade-school/lib/school.ex
@@ -9,20 +9,19 @@ defmodule School do
   Add a student to a particular grade in school.
   """
   @spec add(map, String.t(), integer) :: map
-  def add(db, name, grade) do
-  end
+  def add(db, name, grade), do: Map.update(db, grade, [name], &[name | &1])
 
   @doc """
   Return the names of the students in a particular grade.
   """
   @spec grade(map, integer) :: [String.t()]
-  def grade(db, grade) do
-  end
+  def grade(db, grade), do: Map.get(db, grade, [])
 
   @doc """
   Sorts the school by grade and name.
   """
   @spec sort(map) :: [{integer, [String.t()]}]
-  def sort(db) do
-  end
+  def sort(db), do: Enum.map(db, &sort_grade/1)
+
+  defp sort_grade({grade, students}), do: {grade, Enum.sort(students)}
 end
