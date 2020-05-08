6a697606839511b3a49f260853833cfca243a13b
rapidfireworks <rapidfireworks@gmail.com>
2020-05-08 17:26:01 +0900

    Solve

--- binary-search/lib/binary_search.ex
+++ binary-search/lib/binary_search.ex
@@ -18,6 +18,18 @@ defmodule BinarySearch do
   """
 
   @spec search(tuple, integer) :: {:ok, integer} | :not_found
-  def search(numbers, key) do
+  def search(numbers, key), do: search(numbers, key, 0, tuple_size(numbers))
+
+  defp search(numbers, key, index, count) when count > 0 do
+    offset = div(count, 2)
+    target_index = index + offset
+
+    case elem(numbers, target_index) do
+      ^key -> {:ok, target_index}
+      number when key < number -> search(numbers, key, index, offset)
+      number when key > number -> search(numbers, key, index + count - offset, offset)
+    end
   end
+
+  defp search(_numbers, _key, _index, _count), do: :not_found
 end
